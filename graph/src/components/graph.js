89721896975d8a44390aa516b1a19280cbc2db3a
yanhengfu <yanhengfu@jimengio.com>
2023-03-21 18:31:45 +0800

    1231

---
+++ graph/src/components/graph.js
@@ -0,0 +1,30 @@
+import React, { useRef, useEffect } from 'react'
+import ReactDOM from 'react-dom'
+import data from './mockdata'
+
+export default function Graph() {
+    const boxRef = useRef()
+
+    useEffect(() => {
+    }, [])
+
+
+    const renderList = () => {
+        const box = document.getElementById('box')
+
+
+        Object.keys(data).map()
+
+
+
+    }
+
+
+    return (
+        <div id='box'>
+
+        </div>
+    )
+}
+
+
---
+++ graph/src/components/mockdata.js
@@ -0,0 +1,17 @@
+module.exports = {
+    title: 'a',
+    children: [
+        {
+            title: 'b',
+            children: []
+        },
+        {
+            title: 'b',
+            children: []
+        },
+        {
+            title: 'b',
+            children: []
+        }
+    ]
+}
